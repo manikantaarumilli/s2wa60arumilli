bf3ebb394ed277cc986ad991f4a4ec2d2435c695
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2022-03-30 15:10:19 -0500

    new endpoint

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
-app.use('/mycomputation', mycomputationRouter);
+app.use('/computation', mycomputationRouter);
 
 
 // catch 404 and forward to error handler
